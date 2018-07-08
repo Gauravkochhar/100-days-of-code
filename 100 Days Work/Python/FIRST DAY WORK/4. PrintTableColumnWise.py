9bab08d8250bbae1704740c6608a71b702cac3c7
Gaurav Kochhar <gauravkochhar8822@gmail.com>
2018-07-08 23:38:20 +0530

    Print Table ColumWise Solved

---
+++ 100 Days Work/Python/FIRST DAY WORK/4. PrintTableColumnWise.py
@@ -0,0 +1,8 @@
+for x in range(1,11):
+    print(x,end="\t")
+print('\n___________________________________________')
+for y in range(1,11):
+
+    for x in range(1,11):
+         print(x*y, end="\t")
+    print("\n")
