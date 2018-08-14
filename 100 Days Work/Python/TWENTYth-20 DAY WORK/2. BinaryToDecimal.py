5c1e13f7deabb40561b29cae8c9f7a7991914c82
Gaurav Kochhar <gauravkochhar8822@gmail.com>
2018-08-14 21:40:19 +0530

    Binary To Decimal Problem Is Solved.

---
+++ 100 Days Work/Python/TWENTYth-20 DAY WORK/2. BinaryToDecimal.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a binary number to get Decimal Number: " ))
+base=1
+dv=0
+while(n>0):
+    rem=n%10
+    dv=dv+rem*base
+    n=int(n/10)
+    base=base*2
+print("Binary Number is: ",dv)
