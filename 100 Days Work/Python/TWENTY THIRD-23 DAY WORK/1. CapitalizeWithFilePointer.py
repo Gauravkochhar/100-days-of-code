7fe7bf93c29ba397b1289a178661b0ac7b7237b2
Gaurav Kochhar <gauravkochhar8822@gmail.com>
2018-08-21 19:48:34 +0530

    Capitalize Problem Solved For Each Word In a Sentence.

---
+++ 100 Days Work/Python/TWENTY THIRD-23 DAY WORK/1. CapitalizeWithFilePointer.py
@@ -0,0 +1,25 @@
+"""
+Sample Input->  chris alan
+Sample Output-> Chris Alan
+
+"""
+import math
+import os
+import random
+import re
+import sys
+def solve(s):
+    ls=s.split()
+    newls=[]
+    for i in ls:
+        newls.append(i.title())
+    r=""
+    r=' '.join(i for i in newls)
+    return r
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = solve(s)
+    fptr.write(result + '\n')
+    fptr.close()
