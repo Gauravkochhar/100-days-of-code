402f335e2ec1dd92460a5a3239dcd4800ce26fbd
Gaurav Kochhar <gauravkochhar8822@gmail.com>
2018-08-22 14:15:35 +0530

    String Formation Problem is Solved.

---
+++ 100 Days Work/Python/TWENTY FOURth-24 DAY WORK/1. StringFormation.py
@@ -0,0 +1,54 @@
+"""
+
+Sample Input1= 10
+
+Sample Output1=
+
+------------------j------------------
+----------------j-i-j----------------
+--------------j-i-h-i-j--------------
+------------j-i-h-g-h-i-j------------
+----------j-i-h-g-f-g-h-i-j----------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+----------j-i-h-g-f-g-h-i-j----------
+------------j-i-h-g-h-i-j------------
+--------------j-i-h-i-j--------------
+----------------j-i-j----------------
+------------------j------------------
+
+
+Sample Input2= 5
+
+Sample Output2=
+
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+"""
+
+
+import string
+def print_rangoli(size):
+    alpha=string.ascii_lowercase
+    L=[]
+    for i in range(n):
+        s="-".join(alpha[i:n])
+        L.append((s[::-1]+s[1:]).center(4*n-3,"-"))
+    print('\n'.join(L[::-1]+L[1:]))
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
