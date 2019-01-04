37fccbf257749e035ecbb4ed3700ac5fb84c7e1a
Gaurav Kochhar <gauravkochhar8822@gmail.com>
2019-01-05 00:09:07 +0530

    Pattern is solved.

---
+++ 100 Days Work/C Programming/5. Fifth Day/1. Pattern1.c
@@ -0,0 +1,29 @@
+*     *                                                                                                                        
+ *   *                                                                                                                         
+  * *                                                                                                                          
+   *       
+
+
+#include<stdio.h>
+void main()
+{
+    int i,j,k,l;
+    printf("Enter the value of the pattern size :");
+    scanf("%d",&l);
+    for(i=4;i>=1;i--){
+        for(j=4;j>i;j--){
+            printf(" ");
+        }
+        for(k=1;k<(2*i);k++){
+            if(k>1 && k<(2*i)-1){
+              printf(" ");  
+            }
+            else{
+               printf("*"); 
+            }
+            
+        }
+        printf("\n");
+    }
+    
+}
